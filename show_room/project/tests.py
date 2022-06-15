27d0aced1c846e37d08da7b63161c86589180097
haimy254 <haimanaare@gmail.com>
2022-06-15 15:14:42 +0300

    added test

--- show_room/project/tests.py
+++ show_room/project/tests.py
@@ -8,4 +8,21 @@ class ProfileTestCase(TestCase):
         self.profile = Profile( image='image.jpg', bio='lovely',contact='07897654')
         
     def test_instance(self):
-        pass
+        self.assertTrue(isinstance(self.profile,Profile))
+        
+    def test_save_method(self):
+        self.profile.save() 
+        profile = Profile.objects.all()
+        self.assertTrue(len(profile)>0)
+        
+class ProjectTestClass(TestCase):
+    
+    def setUp(self):
+        self.project = Project(image = 'image.jpg', title = 'project', description = 'text')
+    
+             
+    def test_instance(self):
+        self.assertTrue(isinstance(self.project,Project))  
+        
+       
+    
